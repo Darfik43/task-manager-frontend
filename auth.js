7e9cb7d1722a5c599947b457a2a29cb91c7f5e1f
Darfik43 <darfik43@gmail.com>
2024-12-29 02:40:30 +0300

    feat: add specific url for request

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ function sendAuthRequest() {
         const passwordInput = document.getElementById("passwordInput").value;
 
         try {
-            const response = await fetch("api/v1/auth/login", {
+            const response = await fetch("http://localhost:8080/api/v1/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
