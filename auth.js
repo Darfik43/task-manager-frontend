5cbbc4a57c3f8e382dca5d637f3f6a238f32ebcb
Darfik43 <darfik43@gmail.com>
2024-12-29 02:22:00 +0300

    feat: updateEventListener to listen permanently after page loaded

--- auth.js
+++ auth.js
@@ -1,35 +1,35 @@
 function sendAuthRequest() {
-    document.addEventListener("DOMContentLoaded", () => {
-        const loginForm = document.getElementById("loginForm");
+    const loginForm = document.getElementById("loginForm");
 
-        loginForm.addEventListener("submit", async (event) => {
-            event.preventDefault();
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
 
-            const loginInput = document.getElementById("loginInput").value;
-            const passwordInput = document.getElementById("passwordInput").value;
+        const loginInput = document.getElementById("loginInput").value;
+        const passwordInput = document.getElementById("passwordInput").value;
 
-            try {
-                const response = await fetch("api/v1/auth/login", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        login: loginInput,
-                        password: passwordInput
-                    })
-                });
+        try {
+            const response = await fetch("api/v1/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    login: loginInput,
+                    password: passwordInput
+                })
+            });
 
-                if (!response.ok) {
-                    throw new Error(`Ошибка: ${response.status}`);
-                }
-
-                const data = await response.json();
-                alert("Успешный вход: " + JSON.stringify(data));
-            } catch (error) {
-                console.error("Ошибка при авторизации:", error);
-                alert("Не удалось войти. Проверьте логин и пароль.");
+            if (!response.ok) {
+                throw new Error(`Ошибка: ${response.status}`);
             }
-        });
+
+            const data = await response.json();
+            alert("Успешный вход: " + JSON.stringify(data));
+        } catch (error) {
+            console.error("Ошибка при авторизации:", error);
+            alert("Не удалось войти. Проверьте логин и пароль.");
+        }
     });
 }
+
+document.addEventListener("DOMContentLoaded", sendAuthRequest);
