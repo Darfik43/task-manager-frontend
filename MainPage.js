cc201c443d105ae65db9e003247f86410f8e8697
Darfik43 <darfik43@gmail.com>
2024-12-30 23:42:54 +0300

    feat: update paths

--- MainPage.js
+++ MainPage.js
@@ -1,5 +1,5 @@
-import AuthService from "./UserService.js";
-import HeaderController from "./headerController.js";
+import AuthService from "/UserService.js";
+import HeaderController from "/HeaderController.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const authService = new AuthService("http://localhost:8080/api/v1");
