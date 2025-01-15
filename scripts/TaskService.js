5e75afa3693281e63886dc70cea83e6532d27f5a
Darfik43 <darfik43@gmail.com>
2025-01-15 20:46:46 +0300

    style: add todos

--- scripts/TaskService.js
+++ scripts/TaskService.js
@@ -2,6 +2,8 @@ export default class TaskService {
 
 
     async getUserTasks() {
-
+        // TODO ближайшие задачи:
+        // рендер всех задач на фронте
+        // обработка добавления/удаления/редактирования таски
     }
 }
