688a5822b74431cf65f492930fff2e92147e1b79
Darfik43 <darfik43@gmail.com>
2025-01-15 21:03:04 +0300

    feat: add task methods for front

--- scripts/TaskService.js
+++ scripts/TaskService.js
@@ -6,4 +6,20 @@ export default class TaskService {
         // рендер всех задач на фронте
         // обработка добавления/удаления/редактирования таски
     }
+
+    async createTask() {
+
+    }
+
+    async deleteTask() {
+
+    }
+
+    async editTask() {
+
+    }
+
+    async getTask() {
+
+    }
 }
